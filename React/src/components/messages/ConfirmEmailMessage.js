a443473876bb99d41d531c68c6217b9d9d1ea620
Alexko30 <rank2505@gmail.com>
2018-06-18 02:44:58 +0300

    Dashboar init page

---
+++ React/src/components/messages/ConfirmEmailMessage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react'
+
+const ConfirmEmailMessage = () => (
+  <Message info>
+    <Message.Header>please, verify your emil to unlock awesomeness</Message.Header>
+  </Message>
+);
+
+export default ConfirmEmailMessage;
